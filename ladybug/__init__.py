1119526c589a8daa71a0bda4520a14da3b7c0332
Spencer <spencer.michael.q@gmail.com>
2017-08-23 21:04:57 +0800

    __init__ based on previous study comments

--- ladybug/__init__.py
+++ ladybug/__init__.py
@@ -1,4 +1,7 @@
+"""Ladybug library for Grasshopper."""
 import sys
 
 # This is a variable to check if the library is a [+] library.
-setattr(sys.modules[__name__], 'isplus', False)
+setattr(sys.modules[__name__], 'isplus', True)
+# sys.modules simply makes it so that the the ladybug package would have an isplus attribute set to true
+# i.e. ladybug.isplus = True
